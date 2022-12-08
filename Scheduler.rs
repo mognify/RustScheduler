296f095ab39f251b65beb75680b038e19d0f8f35
Levi <levimogford@gmail.com>
2022-12-08 12:36:03 -0600

    Update Scheduler.rs

--- Scheduler.rs
+++ Scheduler.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let (piss, shit, eat) = (5, 60, 20);
+    let (eins, zwei, eat) = (5, 60, 20);
     let (teeth, shave, shower, dress) = (5, 15, 30, 15);
     let (run, lift, pack, sleep, work) = (30, 60, 15, (10*60), (10.5*60));
     let (laundryStart, laundryTransfer, laundryFinish, loadDuration) = (10, 10, 10, 60);
     let (trash, dishes) = (15, 15);
+    
+    let transit = 5;
     
     let sleepMin = 8*60;
     let sleepMax = 10*60;
@@ -12,4 +14,11 @@ fn main() {
     // AM/PM? Just woke up, or end of day?
     // assign start time, end time
     // checkbox list of things to do
+    let stdSchedule = [transit, eins, zwei, laundryStart]
+    
+    // all of these should be objects tbh...
+    // async bool OR cooldown (must be returned to AFTER, not before, but AFTER x minutes)
+    // time allotted
+    // autoAM / autoPM
+    // 
 }
