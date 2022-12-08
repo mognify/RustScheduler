394009c568db6d71aa47ecb4a1f73f1a738f02d8
Levi <levimogford@gmail.com>
2022-12-08 11:51:05 -0600

    Update Scheduler.rs

--- Scheduler.rs
+++ Scheduler.rs
@@ -2,7 +2,14 @@ fn main() {
     let (piss, shit, eat) = (5, 60, 20);
     let (teeth, shave, shower, dress) = (5, 15, 30, 15);
     let (run, lift, pack, sleep, work) = (30, 60, 15, (10*60), (10.5*60));
-    let (laundryStart, laundryTransfer, laundryFinish) = (10, 10, 10);
+    let (laundryStart, laundryTransfer, laundryFinish, loadDuration) = (10, 10, 10, 60);
     let (trash, dishes) = (15, 15);
     
+    let sleepMin = 8*60;
+    let sleepMax = 10*60;
+    
+    // get system current time
+    // AM/PM? Just woke up, or end of day?
+    // assign start time, end time
+    // checkbox list of things to do
 }
