4ee7574d5d495962774c539ef10b9065ac1a1b92
Levi <levimogford@gmail.com>
2022-12-08 10:56:14 -0600

    Create Scheduler.rs

---
+++ Scheduler.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let (eins, zwei, eat) = (5, 60, 20);
+    let (teeth, shave, shower, dress) = (5, 15, 30, 15);
+    let (run, lift, pack, sleep) = (30, 60, 15, (10*60));
+    let (laundryStart, laundryTransfer, laundryFinish) = (10, 10, 10);
+    let (trash, dishes) = (15, 15);
+}
