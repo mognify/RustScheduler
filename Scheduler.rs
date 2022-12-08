cfb8a1869dee3d5d39617330f28a13f16495aef0
Levi <levimogford@gmail.com>
2022-12-08 10:58:25 -0600

    Update Scheduler.rs

--- Scheduler.rs
+++ Scheduler.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let (eins, zwei, eat) = (5, 60, 20);
+    let (piss, shit, eat) = (5, 60, 20);
     let (teeth, shave, shower, dress) = (5, 15, 30, 15);
-    let (run, lift, pack, sleep) = (30, 60, 15, (10*60));
+    let (run, lift, pack, sleep, work) = (30, 60, 15, (10*60), (10.5*60));
     let (laundryStart, laundryTransfer, laundryFinish) = (10, 10, 10);
     let (trash, dishes) = (15, 15);
+    
 }
